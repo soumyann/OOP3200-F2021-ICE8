c03cc4b5848557803af2d9b965cb1ba072241ed5
soumyann <soumya.narayanamandiramnarayan@dcmail.ca>
2021-11-12 15:42:11 -0500

    Validation for age

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -6,6 +6,9 @@
 
 package ca.durhamcollege;
 
+import com.sun.jdi.request.ExceptionRequest;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -24,12 +27,37 @@ public class Main
 
         //Age
         float age = 0.0f;
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
-        System.out.println();
+        boolean isValidInput = false; //SENTINEL Variable
+        do
+        {
+            System.out.print("Please enter your age: ");
+            try
+            {
+                age = keyboard.nextFloat();
+                isValidInput = true;
 
-        //Output
-        System.out.printf("You entered: %s \n", name);
-        System.out.printf("You entered: %.2f \n", age);
+                if ((age <= 0.0f) ||  (age >=120.0))
+                {
+                    //Output
+
+                    System.out.println("Error: You must enter an age greater than 0.0 and less than 120 years old.");
+                    keyboard.nextLine();
+                    isValidInput = false;
+
+                    /// System.out.printf("You entered: %s \n", name);
+                    //System.out.printf("You entered: %.2f \n", age);
+                }
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must enter a valid floating-point number");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+
+        }
+        while(!isValidInput);
+
     }
 }
+
