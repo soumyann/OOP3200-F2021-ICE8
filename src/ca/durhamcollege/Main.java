9036bae0e1bc0b054e244ea83535f08684f64a29
soumyann <soumya.narayanamandiramnarayan@dcmail.ca>
2021-11-12 12:51:52 -0500

    Updated basic input and output.

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,22 @@
+/*
+*Author : Soumya N.Narayananunni
+* Date: Nov 12,2021
+* File: ICE8*
+ */
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name:");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s ", name);
     }
 }
